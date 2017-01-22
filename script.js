fc0b94a775ad96636f37e52a51be394576a12abc
mitrikyle <mitrikyle@yahoo.com>
2017-01-22 06:27:17 -0800

    fixed loading

--- script.js
+++ script.js
@@ -120,10 +120,31 @@ require([
     });
 
     $("#btn").click(function() {
+
         $("#slide").addClass("enabled");
         $("#slide").removeClass("disabled");
         $("#overlay").addClass("enabled");
         $("#overlay").removeClass("disabled");
+        $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=tanzeelak&api_key=8fa0affd07885c7c5d5df7bf1f61a288&limit=2&format=json&callback=?", function(data) {
+            var songTitle, artist, songTitle2, artist2, id, songhtml, embedHtml;
+            var html = ''; // we declare the variable that we'll be using to store our information
+            var counter = 1; // we declare a counter variable to use with the if statement in order to limit the result to 1
+            $.each(data.recenttracks.track, function(i, item) {
+                if(counter == 1) {
+                    songTitle = item.name;
+                    artist = item.artist['#text'];
+                    html += 'Currently listening to: <span><a href="' + item.url + '" target="_blank">' + songTitle + '</a> - ' + artist + '</span>';
+                    console.log("haha" + html);
+                    console.log(songTitle);
+                } // close the if statement
+                counter++ // add 1 to the counter variable each time the each loop runs
+            }); // close each loop
+            $('.listening-to h5').append(html); // print the information to the document - here I look for the h5 tag inside the div with a class of 'listening-to' and use the jQuery append method to insert the information we've stored in the html variable inside the h5 tag.
+            $('.songTitle').empty();
+            $('.songArtist').empty();
+            $('.songTitle').append(songTitle);
+            $('.songArtist').append(artist);
+        });
     });
 
     $("#landingButton").click(function() {
